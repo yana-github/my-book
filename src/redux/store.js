8b2e3b0190a67854f1a8179e152fac604aa7faab
Yana <yana.0.yakimchuk@gmail.com>
2022-12-06 23:47:06 +0300

    add Redux Persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,22 +1,23 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
 import thunk from "redux-thunk";
 
 import bookReducer from "./book/reducer";
 import themeReducer from "./theme/reducer";
 
+const persistConfig = {
+  key: "book",
+  storage,
+};
+
 const reducers = combineReducers({
-  book: bookReducer,
+  book: persistReducer(persistConfig, bookReducer),
   theme: themeReducer,
 });
 
+let store = createStore(reducers, applyMiddleware(thunk));
+let persistor = persistStore(store);
 
-const persistConfig = {
-    key: 'root',
-    storage,
-  }
-   
-
-export default createStore(reducers, applyMiddleware(thunk));
+export {store, persistor};
